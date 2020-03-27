f54fb732b2af821317101ef2bb5bf8de88b9ea76
Thomas Huguet <thomas.huguet@cpnv.ch>
2020-03-27 10:25:45 +0100

    admin view DB Managed

--- CurrentFile/controler/bdd.php
+++ CurrentFile/controler/bdd.php
@@ -8,42 +8,246 @@
 
 function displayVM()
 {
-    $_GET['action'] = "vm";
-    require 'view/vm.php';
+    if(isset($_SESSION['userType']))
+    {
+        switch ($_SESSION['userType'])
+        {
+            case 1:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+            case 2:
+                $_GET['action'] = "vm";
+                require 'view/vm.php';
+                break;
+            case 3:
+                $_GET['action'] = "vm";
+                require 'view/vm.php';
+                break;
+            case 4:
+                $_GET['action'] = "vm";
+                require 'view/vm.php';
+                break;
+            default:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+        }
+    }
+    else
+    {
+        $_GET['action'] = "home";
+        require 'view/home.php';
+    }
 }
 
 function displayBackup()
 {
-    $_GET['action'] = "backup";
-    require 'view/backup.php';
+    if(isset($_SESSION['userType']))
+    {
+        switch ($_SESSION['userType'])
+        {
+            case 1:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+            case 2:
+                $_GET['action'] = "backup";
+                require 'view/backup.php';
+                break;
+            case 3:
+                $_GET['action'] = "backup";
+                require 'view/backup.php';
+                break;
+            case 4:
+                $_GET['action'] = "backup";
+                require 'view/backup.php';
+                break;
+            default:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+        }
+    }
+    else
+    {
+        $_GET['action'] = "home";
+        require 'view/home.php';
+    }
 }
 
 function displayBDDEntity()
 {
-    $_GET['action'] = "entity";
-    require 'view/entity.php';
+    if(isset($_SESSION['userType']))
+    {
+        switch ($_SESSION['userType'])
+        {
+            case 1:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+            case 2:
+                $_GET['action'] = "entity";
+                require 'view/entity.php';
+                break;
+            case 3:
+                $_GET['action'] = "entity";
+                require 'view/entity.php';
+                break;
+            case 4:
+                $_GET['action'] = "entity";
+                require 'view/entity.php';
+                break;
+            default:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+        }
+    }
+    else
+    {
+        $_GET['action'] = "home";
+        require 'view/home.php';
+    }
 }
 
 function displayOS()
 {
-    $_GET['action'] = "os";
-    require 'view/os.php';
+    if(isset($_SESSION['userType']))
+    {
+        switch ($_SESSION['userType'])
+        {
+            case 1:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+            case 2:
+                $_GET['action'] = "os";
+                require 'view/os.php';
+                break;
+            case 3:
+                $_GET['action'] = "os";
+                require 'view/os.php';
+                break;
+            case 4:
+                $_GET['action'] = "os";
+                require 'view/os.php';
+                break;
+            default:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+        }
+    }
+    else
+    {
+        $_GET['action'] = "home";
+        require 'view/home.php';
+    }
 }
 
 function displayPricing()
 {
-    $_GET['action'] = "pricing";
-    require 'view/pricing.php';
+    if(isset($_SESSION['userType']))
+    {
+        switch ($_SESSION['userType'])
+        {
+            case 1:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+            case 2:
+                $_GET['action'] = "pricing";
+                require 'view/pricing.php';
+                break;
+            case 3:
+                $_GET['action'] = "pricing";
+                require 'view/pricing.php';
+                break;
+            case 4:
+                $_GET['action'] = "pricing";
+                require 'view/pricing.php';
+                break;
+            default:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+        }
+    }
+    else
+    {
+        $_GET['action'] = "home";
+        require 'view/home.php';
+    }
 }
 
 function displaySnapshot()
 {
-    $_GET['action'] = "snapshot";
-    require 'view/snapshot.php';
+    if(isset($_SESSION['userType']))
+    {
+        switch ($_SESSION['userType'])
+        {
+            case 1:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+            case 2:
+                $_GET['action'] = "snapshot";
+                require 'view/snapshot.php';
+                break;
+            case 3:
+                $_GET['action'] = "snapshot";
+                require 'view/snapshot.php';
+                break;
+            case 4:
+                $_GET['action'] = "snapshot";
+                require 'view/snapshot.php';
+                break;
+            default:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+        }
+    }
+    else
+    {
+        $_GET['action'] = "home";
+        require 'view/home.php';
+    }
 }
 
 function displayUser()
 {
-    $_GET['action'] = "user";
-    require 'view/user.php';
+    if(isset($_SESSION['userType']))
+    {
+        switch ($_SESSION['userType'])
+        {
+            case 1:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+            case 2:
+                $_GET['action'] = "user";
+                require 'view/user.php';
+                break;
+            case 3:
+                $_GET['action'] = "user";
+                require 'view/user.php';
+                break;
+            case 4:
+                $_GET['action'] = "user";
+                require 'view/user.php';
+                break;
+            default:
+                $_GET['action'] = "home";
+                require 'view/home.php';
+                break;
+        }
+    }
+    else
+    {
+        $_GET['action'] = "home";
+        require 'view/home.php';
+    }
+
 }
