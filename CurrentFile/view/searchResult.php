bdaa3948bf17609275ee818fca2d5ccc85a43455
Pedroletti Michael <pedrolettimichael@gmail.com>
2020-06-08 10:41:41 +0200

    add vm status when we do a research

--- CurrentFile/view/searchResult.php
+++ CurrentFile/view/searchResult.php
@@ -35,21 +35,40 @@ ob_start();
         <?php foreach ($researchResult as $value): ?>
             <tr>
                 <td>
-                    <a href="index.php?action=detailsVM&id=<?php echo $value['id']?>"><button type="submit" class="btn btn-warning"><strong>+</strong></button></a>
+                    <a href="index.php?action=detailsVM&id=<?php echo $value['id'] ?>">
+                        <button type="submit" class="btn btn-warning"
+                                style="background-color: rgba(<?php if ($value['vmStatus'] == 2) {
+                                    echo '40,167,69,0.7';
+                                } elseif ($value['vmStatus'] == 0) {
+                                    echo '255,165,69,0.7';
+                                } elseif ($value['vmStatus'] == 3) {
+                                    echo '233,48,48,0.7';
+                                } else {
+                                    echo '90,90,90,0.7';
+                                } ?>); border-color: rgba(<?php if ($value['vmStatus'] == 2) {
+                                    echo '40,167,69,0.7';
+                                } elseif ($value['vmStatus'] == 0) {
+                                    echo '255,165,69,0.7';
+                                } elseif ($value['vmStatus'] == 3) {
+                                    echo '233,48,48,0.7';
+                                } else {
+                                    echo '90,90,90,0.7';
+                                } ?>);"><strong>+</strong></button>
+                    </a>
                 </td>
-                <td><?php echo $value['name']?></a></td>
-                <td style="min-width: 100px"><?php echo $value['os_id']['1']." ".$value['os_id']['0']?></td>
-                <td style="min-width: 100px"><?php echo $value['dateStart']?></td>
-                <td style="min-width: 100px"><?php echo $value['dateEnd']?></td>
-                <td><?php echo $value['usageType']?></td>
-                <td><?php echo $value['cpu']?></td>
-                <td><?php echo $value['ram']?></td>
-                <td><?php echo $value['disk']?></td>
-                <td><?php echo $value['network']?></td>
-                <td><?php echo $value['userRt']?></td>
-                <td><?php echo $value['entity_id']?></td>
+                <td><?php echo $value['name'] ?></a></td>
+                <td style="min-width: 100px"><?php echo $value['os_id']['1'] . " " . $value['os_id']['0'] ?></td>
+                <td style="min-width: 100px"><?php echo $value['dateStart'] ?></td>
+                <td style="min-width: 100px"><?php echo $value['dateEnd'] ?></td>
+                <td><?php echo $value['usageType'] ?></td>
+                <td><?php echo $value['cpu'] ?></td>
+                <td><?php echo $value['ram'] ?></td>
+                <td><?php echo $value['disk'] ?></td>
+                <td><?php echo $value['network'] ?></td>
+                <td><?php echo $value['userRt'] ?></td>
+                <td><?php echo $value['entity_id'] ?></td>
             </tr>
-        <?php endforeach;?>
+        <?php endforeach; ?>
         </tbody>
     </table>
 </div>
