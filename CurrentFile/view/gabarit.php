37af3af0ac99efecfd9b5aea86d7128da316e0b1
Thomas Huguet <thomas.huguet@cpnv.ch>
2020-08-19 08:06:50 +0200

    correction bug display formManagement

--- CurrentFile/view/gabarit.php
+++ CurrentFile/view/gabarit.php
@@ -425,7 +425,7 @@
                         </div>
                 </a>
                 <!----------------- formManagement ---------------->
-                <a href="index.php?action=formManagement" class="text-decoration-none text-light">
+                <a href="index.php?action=formManagement&array=entity" class="text-decoration-none text-light">
                     <?php if ($_GET['action'] == "formManagement"): ?>
                         <div class="w-100 pt-2 pl-2" style="height: 40px;">
                     <?php else : ?>
@@ -434,7 +434,7 @@
                             <svg class="bi bi-wrench pb-1 mr-1 icons_transition_width" width="2em" height="2em" viewBox="0 0 16 16" fill="white" xmlns="http://www.w3.org/2000/svg" id="icons_8">
                                 <path fill-rule="evenodd" d="M.102 2.223A3.004 3.004 0 0 0 3.78 5.897l6.341 6.252A3.003 3.003 0 0 0 13 16a3 3 0 1 0-.851-5.878L5.897 3.781A3.004 3.004 0 0 0 2.223.1l2.141 2.142L4 4l-1.757.364L.102 2.223zm13.37 9.019L13 11l-.471.242-.529.026-.287.445-.445.287-.026.529L11 13l.242.471.026.529.445.287.287.445.529.026L13 15l.471-.242.529-.026.287-.445.445-.287.026-.529L15 13l-.242-.471-.026-.529-.445-.287-.287-.445-.529-.026z"/>
                             </svg>
-                            <a href="index.php?action=formManagement" class="text-decoration-none text-light w3-animate-right" id="hidden_14">Gestion du formulaire</a>
+                            <a href="index.php?action=formManagement&array=entity" class="text-decoration-none text-light w3-animate-right" id="hidden_14">Gestion du formulaire</a>
                         </div>
                 </a>
                 <!----------------- displayManagementUser ---------------->
